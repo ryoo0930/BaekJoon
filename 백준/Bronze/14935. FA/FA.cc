ebbc3e587ce6dc5e2ca61e7488cf0478a417f1bb
Assva <69245660+ryoo0930@users.noreply.github.com>
2025-07-12 12:16:35 +0900

    [Bronze IV] Title: FA, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void fast_io() 
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+}
+
+int main()
+{
+    fast_io();
+    
+    // string num = 0; cin >> num;
+    cout << "FA";
+
+    return 0;
+}
